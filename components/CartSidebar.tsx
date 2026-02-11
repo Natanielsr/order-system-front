fb41c545100d54ea0a694aff3bc9ee3b592e1b9e
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-11 16:57:39 -0300

    adicionar imagem no sidebar cart itens

--- components/CartSidebar.tsx
+++ components/CartSidebar.tsx
@@ -2,18 +2,23 @@ import { formatCurrency } from "@/utils/format";
 import { useCart } from "@/context/CartContext";
 import Link from "next/link";
 import Button from "./Button";
+import ProductImage from "./ProductImage";
 
 export default function CartSidebar() {
     const { cart, addToCart, removeFromCart, total } = useCart();
 
     return (
-        <div className="fixed top-12 right-0 w-32 h-full shadow-lg border-l border-l-gray-700 overflow-scroll">
+        <div className="fixed top-12 right-0 w-32 h-full shadow-lg border-l border-l-gray-200 overflow-scroll">
 
             {cart.length === 0 ? <div className="py-5 px-2">Seu Carrinho está vázio</div> : (
                 <>
                     {cart.map((item) => (
                         <div key={item.id}>
-                            <div className="ml-2">
+                            <div className="ml-2 mr-2">
+                                <div className="mt-2 flex justify-center items-center">
+                                    <ProductImage product={item} width={100} height={100}></ProductImage>
+                                </div>
+
                                 <strong>{item.name}</strong>
                                 <div style={{ fontSize: "14px", color: "#666" }}>
                                     {item.quantity}x {formatCurrency(item.price)}
@@ -31,7 +36,7 @@ export default function CartSidebar() {
                             {formatCurrency(total)}
                         </span>
                     </div>
-                    <div className="m-1">
+                    <div className="mb-20 m-1">
                         <Link href="/cart">
                             <Button>Ir Para o Carrinho</Button>
                         </Link>
