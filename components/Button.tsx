2a89e927da7b900b728837275f2c422371ce23f8
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-11 17:18:17 -0300

    remove props from button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -16,10 +16,8 @@ export default function Button({
             {...props}
             className={`
         bg-white text-black
-        px-6 py-2
+        px-2 py-2
         rounded-4xl
-        font-medium
-        text-sm 
         border border-black
         shadow-sm
         transition-all duration-200
