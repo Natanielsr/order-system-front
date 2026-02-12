83d812cce3872daab51277c8a546a7f569ab39e8
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-12 12:43:22 -0300

    add authprovider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import App from "@/components/App";
 import { CartProvider } from "@/context/CartContext";
+import { AuthProvider } from "@/context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <CartProvider>
-          <App>{children}</App>
-        </CartProvider>
+        <AuthProvider>
+          <CartProvider>
+            <App>{children}</App>
+          </CartProvider>
+        </AuthProvider>
       </body>
     </html>
   );
