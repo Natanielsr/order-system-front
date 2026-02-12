e283086cdccf67ebd82830a8dcdbe6f747be7e81
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-12 16:21:02 -0300

    bg-gray and pt-12 in app component

--- components/App.tsx
+++ components/App.tsx
@@ -14,9 +14,9 @@ export default function App({ children }: AppProps) {
 
     return (
         <div>
-            <div className={`${isOpen ? "md:mr-32" : "md:mr-0"} mt-12`}>
+            <div className={`${isOpen ? "md:mr-32" : "md:mr-0"} pt-12`}>
                 <Header />
-                <main className="p-4">
+                <main className="p-4 bg-gray-50">
                     {children}
                 </main>
             </div>
