31e8d4961a9913644484f45f14aa485db635fb32
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-12 12:44:32 -0300

    create login status component

---
+++ components/LoginStatus.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useAuth } from "@/context/AuthContext";
+import { LogInIcon, LogOut } from "lucide-react";
+import Link from "next/link";
+import router from "next/router";
+import { useEffect } from "react";
+
+export default function LoginStatus() {
+    const { user, isAuthenticated, loading, logout } = useAuth();
+
+    useEffect(() => {
+        if (!loading && !isAuthenticated) {
+            //router.push("/login");
+        }
+    }, [loading, isAuthenticated, router]);
+
+    return <div className="flex">
+        {isAuthenticated ? (
+            <>
+                <span className="text-sm mr-2">
+                    Olá, {user?.unique_name}
+                </span>
+
+                <button
+                    onClick={logout}
+                    className='flex items-center gap-2 bg-red-600 hover:bg-red-700 px-4 py-2 rounded-lg transition-colors mr-2'>
+                    <LogOut size={16} />
+                    Sair
+                </button>
+            </>
+        ) : (
+            <Link href="/login">
+                <button
+                    className='flex items-center gap-2 bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg transition-colors mr-2'>
+                    <LogInIcon size={16} />
+                    Login
+                </button>
+            </Link>
+        )}
+    </div>
+}
