6d19f0185af65fb64feea6a6c116e0df20dbe6e7
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-12 16:00:28 -0300

    add responsive  buttons in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,9 @@
 import { useCart } from '@/context/CartContext';
-import { ShoppingCart } from 'lucide-react';
+import { Menu, ShoppingCart } from 'lucide-react';
 import Link from 'next/link';
 import Button from './Button';
 import LoginStatus from './LoginStatus';
+import MobileSidebar from './MobileSidebar';
 
 export default function Header() {
 
@@ -11,22 +12,30 @@ export default function Header() {
     return (
         <header className="fixed top-0 left-0 w-full z-50">
             <nav className='flex justify-between items-center h-12 px-4 shadow-md bg-gray-800 text-white'>
-                <Link href="/" className="text-lg font-bold">
-                    <Button>Lojinha.com</Button>
-                </Link>
-                <div className='flex'>
-
-                    <LoginStatus></LoginStatus>
-                    <Link href="/cart">
-                        <button
-                            className='cursor-pointer flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg transition-colors'>
-                            <ShoppingCart></ShoppingCart>
-                            <span>Carrinho</span>
-                            <span className=''>{totalItens}</span>
-                        </button>
-                    </Link>
-
+                <div className="z-10">
+                    <MobileSidebar></MobileSidebar>
                 </div>
+                <div className="absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0 md:mr-auto">
+                    <Link href="/" className="text-lg font-bold">
+                        <Button>Lojinha.com</Button>
+                    </Link>
+                </div>
+                <div className='hidden md:block'>
+                    <div className='flex'>
+
+                        <LoginStatus></LoginStatus>
+                        <Link href="/cart">
+                            <button
+                                className='cursor-pointer flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg transition-colors'>
+                                <ShoppingCart></ShoppingCart>
+                                <span>Carrinho</span>
+                                <span className=''>{totalItens}</span>
+                            </button>
+                        </Link>
+
+                    </div>
+                </div>
+
             </nav>
         </header >
     )
